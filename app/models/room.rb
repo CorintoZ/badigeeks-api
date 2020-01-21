285c33e2a3570927a364ea0e2ea5e3baa96e3429
allebaria <albert.llebaria.fabrias@gmail.com>
2020-01-21 10:44:54 +0100

    More room model validations (#7)
    * DONE:
     - Description field is not limited anymore.
     - Price valid range is now checked using greater_than and less_than statements instead of length, which is for strings.
     - Title length range amplified up to 75 characters.
    TODO:
     - Associations with other models

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,3 +1,5 @@
 class Room < ApplicationRecord
-    validates :title, :price, :description, presence: true
+  validates :title, :price, :description, presence: true
+  validates :title, length: { minimum: 5, maximum: 80 }
+  validates :price, numericality: { only_integer: true, greater_than: 0, less_than: 10000 }
 end
--- spec/models/room_spec.rb
+++ spec/models/room_spec.rb
@@ -5,5 +5,9 @@ RSpec.describe Room, type: :model do
     should validate_presence_of(:title)
     should validate_presence_of(:price)
     should validate_presence_of(:description)
+    should validate_length_of(:title)
+    should validate_length_of(:description)
+    should validate_length_of(:price)
+    should validate_numericality_of(:price).is_greater_than(0).is_less_than(10000)
   end
 end
