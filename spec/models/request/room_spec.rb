d3f33572bd5f2efe99ca53acaf77c6feccf9cdfa
Daniel <daniel.roig02@estudiant.upf.edu>
2020-01-23 12:02:25 +0100

    RED: test get rooms status 200 added params city lat lon

--- spec/models/request/room_spec.rb
+++ spec/models/request/room_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 RSpec.describe "room API", type: :request do
   describe "GET /rooms" do
-    before { get "/api/v1/rooms" }
+    before { get "/api/v1/rooms?city=barcelona&lat=-123.3&lon=332.3" }
 
     it "returns status code 200" do
       expect(response).to have_http_status(200)
