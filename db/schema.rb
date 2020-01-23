7f86122f56a296d9804cd8ca680fc0ca0187b7b6
Adria1988 <adribuch1988@gmail.com>
2020-01-23 11:54:25 +0100

    add UUIDs for model IDs

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_17_121456) do
+ActiveRecord::Schema.define(version: 2020_01_23_105234) do
 
   # These are extensions that must be enabled in order to support this database
+  enable_extension "pgcrypto"
   enable_extension "plpgsql"
-
-  create_table "rooms", force: :cascade do |t|
-    t.string "title"
-    t.integer "price"
-    t.text "description"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
 
 end
