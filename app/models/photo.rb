e98a78cc80c1cf096c04777211f26a79ec7eb9bf
Adria1988 <adribuch1988@gmail.com>
2020-01-24 09:44:08 +0100

    fix validate photo.rb

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,6 +2,5 @@
 
 class Photo < ApplicationRecord
   belongs_to :room
-  validates :position, presence: true
-  validates :position, numericality: { only_integer: true, greater_than: 0 }
+  validates :position,presence: true,numericality: { only_integer: true, greater_than: 0 }
 end
