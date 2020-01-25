18273cec33def99eebdc8526220a776dcdf08e97
Adria1988 <adribuch1988@gmail.com>
2020-01-25 20:04:07 +0100

    fix user model, add room_size and flat_size

--- spec/factories/room.rb
+++ spec/factories/room.rb
@@ -5,9 +5,11 @@ FactoryBot.define do
     title { Faker::Lorem.word }
     price { Faker::Number.between(from: 1, to: 10_000) }
     description { Faker::Lorem.sentence }
+    kind { 'Barcelona' }
+    room_size { Faker::Number.between(from: 3, to: 15) }
+    flat_size { Faker::Number.between(from: 30, to: 200) }
     lat { Faker::Number.positive }
     lng { Faker::Number.positive }
-    kind { "Barcelona" }
     user_id { nil }
   end
 end
