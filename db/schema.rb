308ece946f8f8814e89520c759d2b5f8a581b99e
Adria1988 <adribuch1988@gmail.com>
2020-01-27 13:14:09 +0100

    fix model user/room

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_23_112023) do
+ActiveRecord::Schema.define(version: 2020_01_27_105100) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -28,7 +28,7 @@ ActiveRecord::Schema.define(version: 2020_01_23_112023) do
 
   create_table "rooms", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "title"
-    t.integer "price"
+    t.float "price"
     t.string "description"
     t.string "kind"
     t.integer "room_size"
