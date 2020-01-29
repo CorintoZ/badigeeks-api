c98fd9bc5090f764a29e7bf6abf9b97f2987dc39
David <david.moreno92@gmail.com>
2020-01-29 17:46:15 +0100

    Custom validations for boundaries

--- app/api/badi/v1/rooms.rb
+++ app/api/badi/v1/rooms.rb
@@ -1,6 +1,7 @@
 module Badi
   module V1
     class Rooms < Grape::API
+      require_relative '../validations/bounds_checker'
       version "v1", using: :path
 
       helpers Helpers::RoomSortingHelpers
@@ -11,17 +12,13 @@ module Badi
       resource :rooms do
         desc "Return rooms"
         params do
-          }
-            v[0].to_f.between?(-180.0, 180.0) && v[1].to_f.between?(-90.0, 90.0) && v[2].to_f.between?(-180.0, 180.0) && v[3].to_f.between?(-90.0,90)
-            v=v.split(',')
-          requires :bounds, type: String, values: ->(v) {
+          requires :bounds, type: String, bounds_checker: true
           requires :page, type: Integer
           requires :size, type: Integer
           optional :sort, type: Integer, values: [2, 3]
         end
         get do
-          present Room.within(params[:bounds]), with: Badi::Entities::RoomList
-          @rooms = Room.within(params[:x1], params[:y1], params[:x2], params[:y2]).paginate(page: params[:page], per_page: params[:size]).order(sorting(params[:sort]))
+          @rooms = Room.within(params[:bounds]).paginate(page: params[:page], per_page: params[:size]).order(sorting(params[:sort]))
           present @rooms, with: Badi::Entities::RoomList
         end
 
