9e65acc4c716331b6b1a05ddf194423f3342fba8
Daniel <daniel.roig02@estudiant.upf.edu>
2020-02-05 10:09:59 +0100

    Similarity with Postgis

--- app/api/badi/v1/helpers/similarity_helpers.rb
+++ app/api/badi/v1/helpers/similarity_helpers.rb
@@ -5,7 +5,8 @@ module Badi
         extend Grape::API::Helpers
 
         def similar_rooms(lng, lat, price, id)
-          @rooms = Room.near([lat, lng], 1, units: :km).where("#{price} <= price and price <= #{price} and id != '#{id}'").limit(3)
+          @rooms = Room.where("ST_DWithin(room_lnglat, ST_SetSRID(ST_MakePoint(lng, lat), 4326), 1.60934)", lng, lat).where("#{price} <= price and price <= #{price} and id != '#{id}'").limit(3)
+          # @rooms = Room.near([lat, lng], 1, units: :km).where("#{price} <= price and price <= #{price} and id != '#{id}'").limit(3)
         end
       end
     end
