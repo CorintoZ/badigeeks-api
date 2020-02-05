2fe4921c9aecf551f277ee7e10e800c8d62476c9
Daniel <daniel.roig02@estudiant.upf.edu>
2020-02-05 09:44:11 +0100

    Similar proximity added

--- app/api/badi/v1/helpers/similarity_helpers.rb
+++ app/api/badi/v1/helpers/similarity_helpers.rb
@@ -5,7 +5,7 @@ module Badi
         extend Grape::API::Helpers
 
         def similar_rooms(lng, lat, price)
-          @rooms = Room.where("#{price} <= price and price <= #{price}")
+          @rooms = Room.near([lat, lng], 1, units: :km).where("#{price} <= price and price <= #{price}")
         end
       end
     end
