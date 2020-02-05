4fb60a221fc5948279e1839f8b4b47039e356216
Daniel <daniel.roig02@estudiant.upf.edu>
2020-02-05 11:03:18 +0100

    Different distance added

--- app/api/badi/v1/helpers/similarity_helpers.rb
+++ app/api/badi/v1/helpers/similarity_helpers.rb
@@ -5,8 +5,18 @@ module Badi
         extend Grape::API::Helpers
 
         def similar_rooms(lng, lat, price, id)
-          @rooms = Room.where("ST_DWithin(room_lnglat, ST_SetSRID(ST_MakePoint(lng, lat), 4326), 1.60934)", lng, lat).where("#{price} <= price and price <= #{price} and id != '#{id}'").limit(3)
+          if !rooms_query(lng, lat, price, id, 1).empty?
+            return @rooms
+          elsif !rooms_query(lng, lat, price, id, 2).empty?
+            return @rooms
+          elsif !rooms_query(lng, lat, price, id, 5).empty?
+            return @rooms
+          end
           # @rooms = Room.near([lat, lng], 1, units: :km).where("#{price} <= price and price <= #{price} and id != '#{id}'").limit(3)
+        end
+
+        def rooms_query(lng, lat, price, id, distance)
+          @rooms = Room.where("ST_DWithin(room_lnglat, ST_SetSRID(ST_MakePoint(lng, lat), 4326), #{distance})", lng, lat).where("price <= #{price + 50}and id != '#{id}'").limit(4)
         end
       end
     end
