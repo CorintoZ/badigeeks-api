b9db6f480bc7adf2ee70d8f7eecf26437813a123
David <david.moreno92@gmail.com>
2020-02-05 16:10:31 +0100

    Update Similarity helper

--- app/api/badi/v1/helpers/similarity_helpers.rb
+++ app/api/badi/v1/helpers/similarity_helpers.rb
@@ -6,17 +6,16 @@ module Badi
 
         def similar_rooms(lng, lat, price, id)
           if !rooms_query(lng, lat, price, id, 1).empty?
-            return @rooms
+            @rooms
           elsif !rooms_query(lng, lat, price, id, 2).empty?
-            return @rooms
+            @rooms
           elsif !rooms_query(lng, lat, price, id, 5).empty?
-            return @rooms
+            @rooms
           end
-          # @rooms = Room.near([lat, lng], 1, units: :km).where("#{price} <= price and price <= #{price} and id != '#{id}'").limit(3)
         end
 
         def rooms_query(lng, lat, price, id, distance)
-          @rooms = Room.where("ST_DWithin(room_lnglat, ST_SetSRID(ST_MakePoint(lng, lat), 4326), #{distance})", lng, lat).where("price <= #{price + 50}and id != '#{id}'").limit(4)
+          @rooms = Room.where("ST_DWithin(room_lnglat, ST_SetSRID(ST_MakePoint(lng, lat), 4326), #{distance})", lng, lat).where("price <= #{price + 50}and id != '#{id}'").limit(3)
         end
       end
     end
