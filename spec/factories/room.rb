3223bc26df333bb19ef2f4fe1c0e4a27f71f0230
Daniel <daniel.roig02@estudiant.upf.edu>
2020-02-05 17:29:54 +0100

    Fixed Gemfile.lock and Factories

--- spec/factories/room.rb
+++ spec/factories/room.rb
@@ -1,14 +1,14 @@
 FactoryBot.define do
   factory :room do
-    title { Faker::Lorem.words(rand(2..500)) }
-    price { Faker::Number.between(from: 1, to: 10_000) }
-    description { Faker::Lorem.words(rand(5..500)) }
-    city { 'Barcelona' }
+    title { Faker::Lorem.words(number: rand(2..100)) }
+    price { Faker::Number.between(from: 11, to: 10_000) }
+    description { Faker::Lorem.words(number: rand(5..100)) }
+    city { "Barcelona" }
     room_size { Faker::Number.between(from: 3, to: 15) }
     flat_size { Faker::Number.between(from: 30, to: 200) }
     lat { 41.398574 }
     lng { 2.152462 }
-    room_lnglat { 'POINT(2.152462 41.398574)' }
+    room_lnglat { "POINT(2.152462 41.398574)" }
     user_id { nil }
   end
 end
--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     name { Faker::Name.name[1..200] }
     date_of_birth { Faker::Date.birthday }
-    bio { Faker::Lorem.words(rand(5..200)) }
+    bio { Faker::Lorem.words(number: rand(5..100)) }
     gender { %w[male female].sample }
   end
 end
